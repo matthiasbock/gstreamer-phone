992222f39b53a95bbdbacdf3ccfe06748fc20af0
root <root@pikachu.(none)>
2013-05-27 10:44:06 +0000

    server/client

---
+++ client.py
@@ -0,0 +1,84 @@
+#! /usr/bin/python
+
+def connect():
+	return True
+
+def negotiate_streams():
+	log("Negotiating session ...")
+	detail("We offer no video.")
+	detail("We offer no audio.")
+	detail("We accept video:")
+	detail2("Codec: H.264")
+	detail3("800x600 @ 30 fps")
+	detail("We accept no audio.")
+	detail(address+" offers video:")
+	detail2("Codec: H.264")
+	detail3("800x600 @ 30 fps - 100 KB/s")
+	detail3("1024x768 @ 30 fps - 300 KB/s")
+	detail(address+" offers no audio.")
+	detail(address+" accepts no video.")
+	detail(address+" accepts no audio.")
+	return True
+
+def negotiate_transport():
+	log("Negotiating ports ...")
+	detail("Remote offers UDP port 5060. Connection test ...")
+	detail2("Remote UDP port 5060 is blocked.")
+	detail("Remote offers UDP port 5070. Connection test ...")
+	detail2("Port ok.")
+	detail("Video will arrive via RTP from remote UDP port 5070.")
+
+def measure_bandwidth():
+	log("Evaluating bandwidth:")
+	detail("Transferring 10 MB of random data ...")
+	detail("100 K/s.")
+	detail("Receiving 10 MB of random data ...")
+	detail("100 K/s.")
+	return True
+
+def select_quality_from_bandwidth():
+	detail("Selecting incoming video resolution:")
+	detail3("800x600 @ 30 fps, average bandwidth: 100 KB/s")
+	detail("Selecting outgoing video resolution:")
+	detail2("no video")
+	return True
+
+def select_lowest_quality():
+	return
+
+def establish_session():
+	log("Establishing session:")
+	detail(address+" will serve video stream via RTP in H264 with 800x60 @ 30 fps for lohmann.no-ip")
+	detail("localhost will play RTP stream from "+address+" UDP:5070 via omxh264dec to framebuffer")
+	# for subsession in gstreamer:
+	#	run
+	detail("Session established. Press Ctrl+C to quit ...")
+	# wait until connection terminated
+	# watch connection running
+	# monitor bandwidth usage
+
+def call(address):
+	if '@' in address:
+		log("Calling user "+user+" on "+provider+" ...")
+	else:
+		log("Calling host "+address+" ...")
+
+	if connect() is None:
+		log("No response. Call aborted.")
+		return None
+	else:
+		log("Connection established.")
+		if negotiate_streams():
+			if measure_bandwidth():
+				if select_quality_from_bandwidth():
+					establish_session()
+				else:
+					log("Fatal: Internet connection is too slow. Call aborted.")
+					return False
+			else:
+				log("Warning: Unable to determine internet bandwidth. Selecting lowest possible quality to ensure connection.")
+				select_lowest_quality()
+				establish_session()
+		else:
+			log("Fatal: Unable to negotiate commonly supported stream format. Call aborted.")
+			return False
