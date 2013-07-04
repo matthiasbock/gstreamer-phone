0fbd66cb392211d7c1a62e66656d1913b4d01597
Urmel <mail@matthiasbock.net>
2013-07-04 17:19:11 +0000

    mDNS for lookup

--- cronscript.py
+++ cronscript.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from socket import gethostbyname
 from subprocess import Popen, PIPE
 from shlex import split
 import urllib, re
@@ -11,14 +12,9 @@ def internetIP():
 	m = re.search('(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)',html_doc)
 	return m.group(0)
 
+# mDNS lookup
 def lanIP():
-	ifconfig = Popen(split('ifconfig wlan0'), stdout=PIPE).communicate()[0].strip()
-	i = ifconfig.find('addr:')
-	if i == -1:
-		return None
-	i += 5
-	j = ifconfig.find('Bcast:')
-	return ifconfig[i:j].strip()
+	return gethostbyname('urmel.local') #open('/etc/hostname').read().strip()+'.local'
 
 default_hosts = """127.0.0.1       localhost urmel
 ::1             localhost ip6-localhost ip6-loopback
@@ -26,13 +22,13 @@ fe00::0         ip6-localnet
 ff00::0         ip6-mcastprefix
 ff02::1         ip6-allnodes
 ff02::2         ip6-allrouters
-
 """
 
 def internet():
 	try:
 		ip = internetIP()
-		print 'My IP address is '+ip
+		#print 'External IP address is '+ip
+
 		f = open('/tmp/hosts','w')
 		f.write(default_hosts+ip+'\tpummeluff')
 		f.close()
@@ -44,19 +40,19 @@ def internet():
 def lan():
 	try:
 		ip = lanIP()
-		print 'My IP address is '+ip
+		#print 'LAN IP address is '+ip
+
 		f = open('/tmp/hosts','w')
 		f.write(default_hosts+ip+'\tpummeluff')
 		f.close()
 
-		Popen(split('scp /tmp/hosts root@urmel:/etc/'))
+		Popen(split('scp /tmp/hosts root@urmel.local:/etc/'))
 	except:
 		pass
 
 if __name__ == '__main__':
-	from socket import gethostbyname
 	try:
-		urmel = gethostbyname('urmel').strip()
+		urmel = gethostbyname('urmel.local').strip()
 		if urmel == '' or lan() is None:
 			raise
 	except:
