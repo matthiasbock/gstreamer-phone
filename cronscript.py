e3533021d0dded1efd2e46f068267ca49248de69
Pummeluff <mail@matthiasbock.net>
2013-07-04 11:08:37 +0000

    refined for LAN

--- cronscript.py
+++ cronscript.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python
 
-from subprocess import Popen
+from subprocess import Popen, PIPE
 from shlex import split
 import urllib, re
 
-def myIP():
+def internetIP():
 	f = urllib.urlopen("http://www.canyouseeme.org/")
 	html_doc = f.read()
 	f.close()
 	m = re.search('(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)',html_doc)
 	return m.group(0)
+
+def lanIP():
+	ifconfig = Popen(split('ifconfig wlan0'), stdout=PIPE).communicate()[0].strip()
+	i = ifconfig.find('addr:')
+	if i == -1:
+		return None
+	i += 5
+	j = ifconfig.find('Bcast:')
+	return ifconfig[i:j].strip()
 
 default_hosts = """127.0.0.1       localhost urmel
 ::1             localhost ip6-localhost ip6-loopback
@@ -17,12 +26,15 @@ fe00::0         ip6-localnet
 ff00::0         ip6-mcastprefix
 ff02::1         ip6-allnodes
 ff02::2         ip6-allrouters
+
 """
 
 def internet():
 	try:
+		ip = internetIP()
+		print ip
 		f = open('/tmp/hosts','w')
-		f.write(default_hosts+'\n'+myIP()+'\tpummeluff')
+		f.write(default_hosts+ip+'\tpummeluff')
 		f.close()
 
 		Popen(split('scp /tmp/hosts root@la-cp386.no-ip.org:/etc/'))
@@ -31,8 +43,10 @@ def internet():
 
 def lan():
 	try:
+		ip = lanIP()
+		print ip
 		f = open('/tmp/hosts','w')
-		f.write(default_hosts)
+		f.write(default_hosts+ip+'\tpummeluff')
 		f.close()
 
 		Popen(split('scp /tmp/hosts root@urmel:/etc/'))
